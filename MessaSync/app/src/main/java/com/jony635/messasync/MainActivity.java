ccb29f1be9c5afd7156a7a96422cddb89ee0c1e4
Jony635 <jony635molina@gmail.com>
2019-07-14 00:25:15 +0200

    dialog for registering user and password

--- MessaSync/app/src/main/java/com/jony635/messasync/MainActivity.java
+++ MessaSync/app/src/main/java/com/jony635/messasync/MainActivity.java
@@ -1,14 +1,17 @@
 package com.jony635.messasync;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPrefs;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -25,8 +29,7 @@ public class MainActivity extends AppCompatActivity {
         if(!sharedPrefs.contains("user"))
         {
             //Register the user
-
-            
+            RegisterUser("");
         }
         else
         {
@@ -35,4 +38,47 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    void RegisterUser(String user)
+    {
+        LayoutInflater inflater = getLayoutInflater();
+
+        final View viewInflated = inflater.inflate(R.layout.edittextdialog, null, false);
+        final EditText userEditText = viewInflated.findViewById(R.id.userEditTextDialog);
+        if(!user.isEmpty())
+            userEditText.setText(user);
+        final EditText passwordEditText = viewInflated.findViewById(R.id.passwordEditTextDialog);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(viewInflated);
+        builder.setTitle("Please, register before using this App");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Apply", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                if(userEditText.getText().length() <= 0)
+                {
+                    Toast.makeText(MainActivity.this, "Error: User field is empty", Toast.LENGTH_SHORT).show();
+                    RegisterUser("");
+                }
+
+                else if(passwordEditText.getText().length() <= 0)
+                {
+                    Toast.makeText(MainActivity.this, "Error: Password field is empty", Toast.LENGTH_SHORT).show();
+                    RegisterUser(userEditText.getText().toString());
+                }
+
+                else
+                {
+                    //Store this pair in the database
+
+                }
+            }
+        });
+
+        builder.create().show();
+    }
+
 }
